1498b53c48efa61a142c07e203fb2714cc49f512
Bastien Nocera <hadess@hadess.net>
2026-06-28 22:24:38 +0200

    Convert manifest to YAML

--- local-build.sh
+++ local-build.sh
@@ -4,6 +4,6 @@ rm -f net.sourceforge.Teo.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build net.sourceforge.Teo.json --repo=_repo
+flatpak-builder --ccache --force-clean _build net.sourceforge.Teo.yaml --repo=_repo
 flatpak build-bundle _repo net.sourceforge.Teo.flatpak net.sourceforge.Teo master
 
